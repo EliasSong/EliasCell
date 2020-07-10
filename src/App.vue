a281b99e0f9ebbe2115057239599a59d2b83afbe
eliasPC <928351020@qq.com>
2020-07-11 00:27:41 +0800

    blog project init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <div class="btn btn-secondary">click me</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
